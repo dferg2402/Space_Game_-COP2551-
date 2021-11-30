7adc9f1011216831c39dc5f5ee50a2b9cce4e0f8
dferg2402 <84884272+dferg2402@users.noreply.github.com>
2021-11-29 20:53:57 -0500

    Engine Accumulator
    Changed parts total method to return a value
    Set method to a variable and put variable inside MessageBox

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Game_Demo
+{
+    class Player
+    {
+        //fields
+        private string _name;
+        private int _coin;
+        private int _engine_parts;
+        private int _total_parts;
+
+        //constructor
+        public Player()
+        {
+            _name = "";
+            _coin = 0;
+            _engine_parts = 0;
+            _total_parts = 0;
+        }
+
+        //properties
+        public string Name { get; set; }
+        public int Coin { get; set; }
+        public int EngineParts { get; set; }
+        public int TotalParts { get; set; }
+
+        //method to accumulate total engine parts
+        public int PartsTotal(int total)
+        {
+            _total_parts += total;
+            return _total_parts;
+        }
+    }
+
+}
